5ec09e20ee69c82268d8d3e51bfd776dfe819f24
Lipták János <janos.liptak@gmail.com>
2018-10-06 11:06:40 +0200

    www

--- TeglatestTerfogat/src/TeglaTest.java
+++ TeglatestTerfogat/src/TeglaTest.java
@@ -6,7 +6,6 @@ public class TeglaTest {
 		int a = Integer.parseInt(args[0]);
 		int b = Integer.parseInt(args[1]);
 		int c = Integer.parseInt(args[2]);
-		
 		System.out.println("Téglatest térfogat:" + a * b * c);
 		System.out.println("Téglatest felszín :" + 2* (a * b) + 2 * (c *b) + 2 * (a *c));		
 
